dcf56aa06f528787789419627631131656fcade0
matt-pasek <mpasek@tm1.edu.pl>
2024-03-23 15:31:22 +0100

    activated endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+import substitutesRouter from './api/substitutes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
+
+app.use('/api/substitutes', substitutesRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
